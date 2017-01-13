951ca0d34369eeb706994bf416aa7a04210301a3
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2017-01-13 12:46:53 +0200

    d

--- js/main.js
+++ js/main.js
@@ -5,6 +5,24 @@ window.params = {
 };
 
 
+/**
+     *
+     * Check if element exist on page
+     *
+     * @param el {string} jQuery object (#popup)
+     *
+     * @return {bool}
+     *
+*/
+function exist(el){
+    if ( $(el).length > 0 ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 jQuery(document).ready(function($) {
 
     /*---------------------------
@@ -195,6 +213,8 @@ jQuery(document).ready(function($) {
         });
     }
 
-    googleMap_initialize(); 
+    if ( exist( '#map_canvas' ) ) {
+        googleMap_initialize();
+    }
 
 }); // end file
